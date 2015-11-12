aa2f901f386e5afd88cf9a5d76f96984d94b11a5
Danielle H <daheber@gmail.com>
2015-11-12 11:40:39 -0800

    Random number of customers per day.

--- shops.js
+++ shops.js
@@ -1,49 +1,22 @@
-//Declaring each shop in a seperate object
+//Creating a donut shop object constructor
 
-        var blueStar = {
-          name: "Blue Star",
-          minCust: 8,
-          maxCust: 43,
-          perCust: 4.5,
-          hrsOpen: 11
-        };
+  var createShop = function(name, minCust, maxCust, perCust, hrsOpen) {
+    this.name = name;
+    this.minCust = minCust;
+    this.maxCust = maxCust;
+    this.perCust = perCust;
+    this.hrsOpen = hrsOpen;
+    this.getCustPerDay = function() {
+        var custPerDay = (Math.floor(Math.random() * (this.maxCust - this.minCust + 1)) + this.minCust);
+        return custPerDay;
+    };
+  }
 
-        var voodoo = {
-          name: "Voodoo",
-          minCust: 4,
-          maxCust: 37,
-          perCust: 2,
-          hrsOpen: 24
-        };
+//Adding specific shop data and pushing into an array that can be looped through
 
-        var cosco = {
-          name: "Cosco",
-          minCust: 9,
-          maxCust: 23,
-          perCust: 6.33,
-          hrsOpen: 11
-        };
-
-        var tonalis = {
-          name: "Tonallis Donuts & Cream",
-          minCust: 2,
-          maxCust: 28,
-          perCust: 1.25,
-          hrsOpen: 17
-        };
-
-        var sesame = {
-          name: "Sesame Donuts",
-          minCust: 8,
-          maxCust: 58,
-          perCust: 3.75,
-          hrsOpen: 24
-        };
-
-//Push shops into an array that can be looped through
-        var shops = new Array();
-        shops.push(blueStar);
-        shops.push(voodoo);
-        shops.push(cosco);
-        shops.push(tonalis);
-        shops.push(sesame);
+  var shops = new Array();
+      shops.push(new createShop("Blue Star", 8, 43, 4.5, 11));
+      shops.push(new createShop("Voodoo", 4, 37, 2, 24));
+      shops.push(new createShop("Cosco", 9, 23, 6.33, 11));
+      shops.push(new createShop("Tonallis Donuts", 2, 28, 1.25, 17));
+      shops.push(new createShop("Sesame", 8, 58, 3.75, 24));
