d630d57658686adaafae73027c2edeed44e00ab9
Danielle H <daheber@gmail.com>
2015-11-21 13:43:53 -0800

    Full cleanup of the onChange functions

--- shops.js
+++ shops.js
@@ -42,7 +42,7 @@ var createShop = function(name, minCust, maxCust, perCust, hrsOpen) {
             secondaryTableData += "<tr><td>" + hour+"</td><td>"+numCust+"</td><td>"+ numDonut + "</td></tr>";
         }
 
-        secondaryTableData+= "<tr><th><input type='number' name='hrsOpen' onChange=hrsOpenUpdate("+ index +") value= "+this.hrsOpen+"></th>";
+        secondaryTableData+= "<tr><th><input type='number' name='hrsOpen' onChange=updateInfo("+ index +") value= "+this.hrsOpen+"></th>";
         secondaryTableData+="<th>"+totalCust+"</th>";
         secondaryTableData+="<th>"+totalDonuts+"</th></tr>";
         return secondaryTableData;
@@ -66,62 +66,31 @@ var createShop = function(name, minCust, maxCust, perCust, hrsOpen) {
   }
 /***Prompts the table info to update when the user changes minCust, maxCust, perCust***/
   function updateInfo(index) {
-    /*alert("shop number: " +index + " value: " + parseFloat(document.getElementsByName('minCust')[index].value));*/
-
     /*get new input value and store it in a variable */
     var newMinCust = parseInt(document.getElementsByName('minCust')[index].value);
     var newMaxCust = parseInt(document.getElementsByName('maxCust')[index].value);
     var newPerCust = parseFloat(document.getElementsByName('perCust')[index].value);
-    /*pass the new input value into this instance for the correct shop*/
-    shops[index].minCust = newMinCust;
-    shops[index].maxCust = newMaxCust;
-    shops[index].perCust = newPerCust;
-    /*Recalulate table*/
-    var displayTable = document.getElementById('table' + index);
-    displayTable.innerHTML = writeNewInfo(index)
-                  + shops[index].perHour(index) + "</table></div>";
+    var newHrsOpen = parseInt(document.getElementsByName('hrsOpen')[index].value);
+      /**Checks hrs open to make sure it's between 1-24**/
+          if ((newHrsOpen > 24) || (newHrsOpen <= 0)) {
+             /*Throw an error message*/
+            alert("Please enter a number between 1 and 24.");
+            document.getElementsByName('hrsOpen')[index].value = shops[index].hrsOpen;
+          }
+          else {
+            /*pass the new input value into this instance for the correct shop*/
+            shops[index].minCust = newMinCust;
+            shops[index].maxCust = newMaxCust;
+            shops[index].perCust = newPerCust;
+            shops[index].hrsOpen = newHrsOpen;
+            /*Redraw the table*/
+            var displayTable = document.getElementById('table' + index);
+            displayTable.innerHTML = writeNewInfo(index)
+            + shops[index].perHour(index) + "</table></div>";
+          }
   }
   /****Updates the innerHTML for the minCust, maxCust, perCust change ***/
   function writeNewInfo(index) {
-      var tableData ="<h2>" + shops[index].name + "</h2>";
-      tableData += "<table><tr>";
-      tableData += "<th>Min Customer</th>";
-      tableData += "<th>Max Customer</th>";
-      tableData += "<th>Avg Donut/Customer</th>";
-      tableData += "</tr></tr>";
-      tableData += "<td><input type='number' name='minCust' onChange=updateInfo("+ index +") value= "+shops[index].minCust+"></td>";
-      tableData += "<td><input type='number' name='maxCust' onChange=updateInfo("+ index +") value=" +shops[index].maxCust+"></td>";
-      tableData += "<td><input type='number' name='perCust' onChange=updateInfo("+ index +") value=" +shops[index].perCust+"></td></tr>";
-      tableData += "<tr><th>Hour</th>";
-      tableData += "<th>Number of Customers</th>";
-      tableData += "<th>Donuts Sold</th></tr>";
-      return tableData;
-    }
-
-
-/***Prompts the table info to update when the user changes the hrs Open number***/
-  function hrsOpenUpdate(index) {
-    /*alert("shop number: " +index + " value: " + parseFloat(document.getElementsByName('hrsOpen')[index].value));*/
-
-    /*get new input value and store it in a variable */
-    var newHrsOpen = parseInt(document.getElementsByName('hrsOpen')[index].value);
-
-    /**Checks to see if hrs open is less than or equal to 24 hrs**/
-    if ((newHrsOpen > 24) || (newHrsOpen <= 0)) {
-      alert("Please enter a number between 1 and 24.");
-      document.getElementsByName('hrsOpen')[index].value = shops[index].hrsOpen;
-    }
-    else {
-    /*pass the new input value into this.minCust for the correct shop*/
-    shops[index].hrsOpen = newHrsOpen;
-    /*Recalulate table*/
-    var displayTable = document.getElementById('table' + index);
-    displayTable.innerHTML = writeNewHrsOpen(index)
-                  + shops[index].perHour(index) + "</table></div>";
-    }
-  }
-  /****Updates the innerHTML for the hrs open update ***/
-  function writeNewHrsOpen(index) {
       var tableData ="<h2>" + shops[index].name + "</h2>";
       tableData += "<table><tr>";
       tableData += "<th>Min Customer</th>";
